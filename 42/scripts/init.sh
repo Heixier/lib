46248822b28473387d823545c355ff1df837b967
Heixier <rzsiah@gmail.com>
2025-04-27 19:54:31 +0800

    make init change wallpaper instead of just copying

--- 42/scripts/init.sh
+++ 42/scripts/init.sh
@@ -2,8 +2,13 @@
 
 HOME="${HOME:-/home/rsiah}"
 
+XWINWRAP="$HOME/.local/bin/xwinwrap"
+CVLC="/usr/bin/cvlc"
+GSETTINGS="/usr/bin/gsettings"
+
 IMAGE="shiroko.png"
 IMAGE_PATH="$HOME/media/walls/static/$IMAGE"
+IMAGE_DEST="$HOME/.local/share/backgrounds/$IMAGE"
 
 LIVE="shiroko.mp4"
 LIVE_PATH="$HOME/media/walls/live/$LIVE"
@@ -11,12 +16,15 @@ LIVE_PATH="$HOME/media/walls/live/$LIVE"
 LOCK="shiroko.gif"
 LOCK_PATH="$HOME/media/walls/live/$LOCK"
 
-XWINWRAP="$HOME/.local/bin/xwinwrap"
-CVLC="/usr/bin/cvlc"
-
-# Setup wallpapers
+# Setup lock screen
 cp "$LOCK_PATH" /tmp/codam-web-greeter-user-wallpaper
-cp "$IMAGE_PATH" $HOME/.local/share/backgrounds/"$IMAGE"
+
+# Setup wallpaper
+cp "$IMAGE_PATH" "$IMAGE_DEST"
+gsettings set org.gnome.desktop.background color-shading-type 'solid'
+gsettings set org.gnome.desktop.background picture-options 'zoom'
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMAGE_DEST"
+gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_DEST"
 
 $XWINWRAP -fs -fdt -ni -b -nf -un -o 1.0 -- $CVLC --no-video-title-show --drawable-xid WID --loop --no-audio "$LIVE_PATH"
 
