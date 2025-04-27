c8c448c2153ba52f0a1bdc2afa0a68030093ae10
Heixier <rzsiah@gmail.com>
2025-04-27 23:10:45 +0800

    add run in background to init.sh

--- 42/scripts/init.sh
+++ 42/scripts/init.sh
@@ -10,14 +10,18 @@ IMAGE="shiroko.png"
 IMAGE_PATH="$HOME/media/walls/static/$IMAGE"
 IMAGE_DEST="$HOME/.local/share/backgrounds/$IMAGE"
 
+ICON="shiroko_icon.gif"
+ICON_PATH="$HOME/media/icons/live/$ICON"
+
 LIVE="shiroko.mp4"
 LIVE_PATH="$HOME/media/walls/live/$LIVE"
 
 LOCK="shiroko.gif"
 LOCK_PATH="$HOME/media/walls/live/$LOCK"
 
-# Setup lock screen
+# Setup lock screen and profile picture
 cp "$LOCK_PATH" /tmp/codam-web-greeter-user-wallpaper
+cp "$ICON_PATH" /tmp/codam-web-greeter-user-avatar
 
 # Setup wallpaper
 cp "$IMAGE_PATH" "$IMAGE_DEST"
@@ -26,7 +30,8 @@ gsettings set org.gnome.desktop.background picture-options 'zoom'
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMAGE_DEST"
 gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_DEST"
 
-$XWINWRAP -fs -fdt -ni -b -nf -un -o 1.0 -- $CVLC --no-video-title-show --drawable-xid WID --loop --no-audio "$LIVE_PATH"
+# Start animation
+$XWINWRAP -fs -fdt -ni -b -nf -un -o 1.0 -- $CVLC --no-video-title-show --drawable-xid WID --loop --no-audio "$LIVE_PATH" >/dev/null 2>&1 &
 
 # Cleanup
 bash ~/Core/malware/pranks/antidotes/kill_parrots.sh
